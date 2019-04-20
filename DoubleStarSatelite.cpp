3792fd3b90979eb78ff73ab8aa79ce8a986c1b89
dyakovri <errorman.gamer@gmail.com>
2019-04-21 00:50:59 +0300

    Motion modelling

---
+++ DoubleStarSatelite.cpp
@@ -0,0 +1,40 @@
+#include "DoubleStarSatelite.hpp"
+
+DoubleStarSatelite::DoubleStarSatelite(double M1, double M2, double m, double L, double x0, double y0, double Vx0, double Vy0) : RKMethod(4)
+{
+    this->M1 = M1;
+    this->M2 = M2;
+    this->m = m;
+
+    this->L = L;
+    this->x1 = L * M2 / (M1+M2);
+    this->x2 = L - x1;
+
+    this->w = std::sqrt(G*(M1+M2)/std::pow(L, 3));
+
+    std::vector<double> Y0(4);
+    Y0[0] = Vx0;
+    Y0[1] = Vy0;
+    Y0[2] = x0;
+    Y0[3] = y0;
+}
+
+std::vector<double> DoubleStarSatelite::F(double time, std::vector<double> &coordinates) {
+    // x'=Vx - FY[0] = Y[2]
+    // y'=Vy - FY[1] = Y[3] 
+    // Vx'=... FY[2] =  
+    // Vy'=... FY[3] =  
+
+    FY[0] = Y[2];
+    FY[1] = Y[3];
+    FY[2] = G*M1*(Y[0]-x1) / std::pow(std::pow(Y[0]-x1,2.) + std::pow(Y[1],2.), 3./2.) 
+          + G*M2*(Y[0]-x2) / std::pow(std::pow(Y[0]-x2,2.) + std::pow(Y[1],2.), 3./2.) 
+          + w*w*Y[0] + 2*Y[3]*w;
+    FY[3] = G*M1*Y[1] / std::pow(std::pow(Y[0]-x1,2.)+ std::pow(Y[1],2.), 3./2.) 
+          + G*M2*Y[1] / std::pow(std::pow(Y[0]-x2,2.)+ std::pow(Y[1],2.), 3./2.) 
+          + w*w*Y[1] - 2*Y[2]*w;
+}
+
+DoubleStarSatelite::~DoubleStarSatelite()
+{
+}
---
+++ DoubleStarSatelite.hpp
@@ -0,0 +1,30 @@
+#include "RKMethod.hpp"
+#include <cmath>
+#include <vector>
+
+#ifndef DoubleStarSateliteCls
+#define DoubleStarSateliteCls
+
+class DoubleStarSatelite : public RKMethod
+{
+private:
+    const double G = 6.67e-11; // м3 с-3 кг-2 Гравитационная постоянная
+
+    double M1, M2, m;   // Массы первой звезды, второй и спутника соотв.
+    double x1, x2, L;   // Расстояния от начала координат до первой звезды, до второй звезды и между звездами
+    double w;           // Угловая скорость вращения системы звезд
+    double x, y;        // Координаты спутника  
+    double Vx, Vy;      // Скорость спутника 
+public:
+    DoubleStarSatelite(double M1, double M2, double m, double L, double x0, double y0, double Vx0, double Vy0);
+    ~DoubleStarSatelite();
+    
+    virtual std::vector<double> F(double t, std::vector<double> &Y);
+
+    double get_x() { return Y[0]; };
+    double get_y() { return Y[1]; };
+    double get_Vx() { return Y[2]; };
+    double get_Vy() { return Y[3]; };
+};
+
+#endif
